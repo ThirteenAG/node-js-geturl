8319d1313bd86667ef198f3da7fb2fbac7a791d4
ThirteenAG <thirteenag@gmail.com>
2016-12-22 17:46:03 +0300

    no params

--- index.js
+++ index.js
@@ -9,31 +9,36 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   var pUrl = url.parse(request.url, true).query['url'];
-  pUrl = pUrl.substr(1, pUrl.length - 2);
   var pSelector = url.parse(request.url, true).query['selector'];
-  pSelector =  pSelector.substr(1, pSelector.length - 2);
 
   console.log("Url: " + pUrl);
   console.log("Selector: " + pSelector);
 
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-  	if (this.readyState === 4) {
-      console.log("Complete.\nBody length: " + this.responseText.length);
-      //console.log("Body:\n" + this.responseText);
-      let $ = cheerio.load(this.responseText);
-      var object = $('<div/>').html($.html()).contents();
-      var link = object.find(pSelector).attr("href");
+  if ((typeof pUrl != 'undefined') && (typeof pSelector != 'undefined'))
+  {
+    pUrl = pUrl.substr(1, pUrl.length - 2);
+    pSelector =  pSelector.substr(1, pSelector.length - 2);
 
-      console.log("link: " + link);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+    	if (this.readyState === 4) {
+        console.log("Complete.\nBody length: " + this.responseText.length);
+        //console.log("Body:\n" + this.responseText);
+        let $ = cheerio.load(this.responseText);
+        var object = $('<div/>').html($.html()).contents();
+        var link = object.find(pSelector).attr("href");
 
-      response.send(link);
-  	}
-  };
-  
-  xhr.open("GET", pUrl);
-  xhr.send();
-  
+        console.log("link: " + link);
+
+        response.send(link);
+    	}
+    };
+
+    xhr.open("GET", pUrl);
+    xhr.send();
+  }
+  else
+    response.send("");
 })
 
 app.listen(app.get('port'), function() {
